5f82361f2fb1926101ef321a90aabfc4ba6d018c
akash shrestha <akash_shrestha01@hotmail.com>
2021-04-01 11:05:10 +0545

    second commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+This is a simple node js file server app.
+
+To use the app
+-Download the folder 
+-Run 'npm install'
+-Run 'npm start'
+
+
+Place the file you want to serve in the 'public' folder.
+Enter the url/filename to access the file
+The file is publicly available and anyone access to the site can access the file.
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.send("Welcome, please enter the correct url with the complete file name !");
+  res.send(`Welcome, please enter the correct url with the complete file name ! 
+  <br> For eg: if your file name is 'image1.jpg' then enter 'http://localhost:4000/image1.jpg'
+  or 'http://yourUrl/image1.jpg'`);
 });
 
 app.listen(4000, () => {
-  console.log('Listening for req on the port 4000 !');
+  console.log('Server running, Listening for req on the port 4000 !');
 });
 
